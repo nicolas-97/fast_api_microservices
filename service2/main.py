f75087ec2151f75d74241fcd8ac71da55cc86f5a
NicoDevel <nicolassabogal85@gmail.com>
2023-10-11 18:55:13 -0500

    start project

---
+++ service2/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+# Crea una instancia de la aplicación FastAPI
+app = FastAPI()
+
+# Define una ruta raíz
+@app.get("/")
+def read_root():
+    return {"message": "¡Bienvenido al microservicio 2 de ejemplo!"}
